9e2f461177b10904493a2b34d3c24818894c6824
Dennis Grönberg <gronberg.dennis@gmail.com>
2016-10-19 11:15:45 +0200

    Update fixed v.2

--- HeadButton/HeadButton/Model Layer/Model.cs
+++ HeadButton/HeadButton/Model Layer/Model.cs
@@ -44,20 +44,18 @@ namespace HeadButton.Model_Layer
 
         public static void Update()
         {
-            string productName = Presenter.newProductName;
-            int unitPrice = Presenter.newUnitPrice;
-            int productIndex = Presenter.productIndex;
-
             SqlConnection conn = new SqlConnection();
             conn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            
-            //en form som hämtar id från "filltextbox"                                       v
-            string sqlQuery = "UPDATE [NORTHWND].[dbo].[Categories] SET ";
-            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
-            conn.Open();
-            SqlDataReader rd = cmd.ExecuteReader();
-            
 
+            string sqlQuery = "UPDATE [NORTHWND].[dbo].[Products] SET [ProductName]=@ProductName, [UnitPrice]=@UnitPrice WHERE [ProductID]=@ProductID";
+            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+            cmd.Parameters.AddWithValue("@ProductName", Presenter.newProductName);
+            cmd.Parameters.AddWithValue("@UnitPrice", Presenter.newUnitPrice);
+            cmd.Parameters.AddWithValue("@ProductID", Presenter.productIndex);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
         }
     }
 }
